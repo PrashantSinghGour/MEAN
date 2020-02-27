5ce32a551839253f50090606a435223b46432ec4
Prashant Singh <prashantsinghgour@gmail.com>
2020-02-27 14:37:03 +0530

    Material Design Added

--- MeanProject/src/app/app.module.ts
+++ MeanProject/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './Posts/post-create/post-create.component';
+import {FormsModule} from '@angular/forms';
 
+// Material Design Dependencies
+import { MatInputModule,MatCardModule,MatButtonModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
